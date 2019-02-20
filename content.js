bd31ffa9cce5e318216f543b99975ea9e1c359a3
andrewtong0 <andrewtong0@gmail.com>
2019-02-20 00:55:37 -0800

    Push correction to enable temporary functionality (disabled URL checking)

--- content.js
+++ content.js
@@ -56,7 +56,7 @@ function saveEntry(submittedValues, dateTime, url){
 // Writes values to page from storage (called from popup.js)
 function writeToPage() {
     chrome.storage.sync.get(['url', 'submissionDate', 'answers'], function (items) {
-        if (validateURL(items.url, url)) {
+        //if (validateURL(items.url, url)) {
             let submissionArea = document.getElementById("problem_body");
             let submissionFields = submissionArea.querySelectorAll('input, select');
             console.log(submissionFields);
@@ -78,7 +78,7 @@ function writeToPage() {
             console.log("Reloaded previous submission");
             console.log(items.submissionDate);
             console.log(items.answers);
-        }
+        //}
     });
 }
 
