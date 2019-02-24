a95e5a295d73c20332a7ad478eae4e2f5b390652
andrewtong0 <andrewtong0@gmail.com>
2019-02-23 22:03:38 -0800

    Added JQuery support files and scripts, added iframe functionality with dialog popup for interactive elements.

--- content.js
+++ content.js
@@ -46,6 +46,7 @@ function augmentSubmissionButton() {
     submit.addEventListener('click', saveSubmission);
 }
 
+// Saves submitted values into Chrome sync storage
 function saveEntry(submittedValues, dateTime, url){
     JSON.stringify(submittedValues);
     chrome.storage.sync.set({'url' : url, 'submissionDate' : dateTime, 'answers' : submittedValues}, function() {
@@ -53,7 +54,7 @@ function saveEntry(submittedValues, dateTime, url){
     });
 }
 
-// Writes values to page from storage (called from popup.js)
+// Writes values to page (in appropriate slots) from storage (called from popup.js) - Rollback feature
 function writeToPage() {
     chrome.storage.sync.get(['url', 'submissionDate', 'answers'], function (items) {
         if (validateURL(items.url, url)) {
@@ -82,9 +83,134 @@ function writeToPage() {
     });
 }
 
+// Validates URL to ensure answers are written to the right question
 function validateURL(storedURL, curURL){
     // URLs for question can vary, but all share same split prior to first ?
     return(curURL.includes(storedURL.split("?")[0]));
 }
 
-window.onload = augmentSubmissionButton;
+window.onload = augmentSubmissionButton; // Injects JavaScript into submission button on page load
+window.onload = createFramePopup;
+
+function showHistory(){
+
+}
+
+function createFramePopup(){
+    let width = 350;
+    let height = 500;
+    let spacing = 65;
+
+    // Dialog Box Setup
+    let dialog1 = document.createElement("div");
+    dialog1.id = "dialog1";
+    dialog1.title = "Toolbox";
+    dialog1.hidden = true;
+
+    // Select menu setup
+    let webSelect = document.createElement("select");
+    webSelect.onchange = setSource;
+    dialog1.appendChild(webSelect);
+
+    let item1 = document.createElement("option");
+    item1.label="Graphing Calculator (Desmos)";
+    item1.value="https://www.desmos.com/calculator";
+    let item2 = document.createElement("option");
+    item2.label="Scientific Calculator (Desmos)";
+    item2.value="https://www.desmos.com/scientific";
+    let item3 = document.createElement("option");
+    item3.label="Derivative Calculator (Wolfram Alpha)";
+    item3.value="https://www.wolframalpha.com/widget/widgetPopup.jsp?p=v&id=f4ccdf86d504bc91f8652fa6d7b76db6&title=Derivative+Calculator&theme=blue&i0=1st&i1=-6%2Fx&podSelect&includepodid=Input&podstate=Input__Step-by-step+solution&showAssumptions=1&showWarnings=1";
+    let item4 = document.createElement("option");
+    item4.label="Integral Calculator (Wolfram Alpha)";
+    item4.value="www.wolframalpha.com/widget/widget.jsp?id=539ef7f650041a2d28cadf9ad0292bf2";
+    let item5 = document.createElement("option");
+    item5.label="Matrix Calculator";
+    item5.value="https://matrixcalc.org/en/";
+    webSelect.appendChild(item1);
+    webSelect.appendChild(item2);
+    webSelect.appendChild(item3);
+    webSelect.appendChild(item4);
+    webSelect.appendChild(item5);
+
+    // iframe Setup
+    let iframe = document.createElement("iframe");
+    iframe.src = "https://www.desmos.com/calculator";
+    iframe.style.border = 'none';
+    iframe.style.width = width.toString() + "px";
+    iframe.style.height = height.toString() + "px";
+    dialog1.appendChild(iframe);
+
+    let script = document.createElement("script");
+    script.style = "{visibility: hidden}";
+    dialog1.appendChild(script);
+
+    // Function run when selection made to dropdown in iframe
+    function setSource(){
+        let item = webSelect.options[webSelect.selectedIndex];
+        iframe.src = item.value;
+        console.log(item.label);
+        if (item.label == "Graphing Calculator (Desmos)") {
+            setDimensions(350, 500);
+            iframe.style = "{visibility: visible}";
+            script.style = "{visibility: hidden}";
+        }
+        else if (item.label == "Scientific Calculator (Desmos)") {
+            setDimensions(350, 500);
+            iframe.style = "{visibility: visible}";
+            script.style = "{visibility: hidden}";
+        }
+        else if (item.label == "Derivative Calculator (Wolfram Alpha)") {
+            setDimensions(800, 500);
+            iframe.style = "{visibility: hidden}";
+            script.style = "{visibility: visible}";
+        }
+        else if (item.label == "Integral Calculator (Wolfram Alpha)"){
+            setDimensions(800, 500);
+            iframe.style = "{visibility: hidden}";
+            script.style = "{visibility: visible}";
+        }
+        else if (item.label == "Matrix Calculator"){
+            setDimensions(800, 500);
+            iframe.style = "{visibility: visible}";
+            script.style = "{visibility: hidden}";
+        }
+    }
+
+    function setDimensions(newWidth, newHeight){
+        $( "#dialog1" ).dialog({
+            width: newWidth + spacing,
+            height: newHeight + spacing + 40 // 40px to compensate for menu
+        });
+        iframe.width = newWidth;
+        iframe.height = newHeight;
+        console.log(newWidth);
+        console.log(newHeight);
+    }
+
+    // Button for toggling visibility
+    let button = document.createElement('input');
+    button.type = 'submit';
+    button.id = "opener";
+    button.className = "btn btn-primary";
+    button.textContent = "Open Tool Box";
+
+    // Inserting elements onto web page
+    let insertionDiv = document.getElementById("bs-container");
+    document.body.insertBefore(dialog1, insertionDiv);
+    document.body.insertBefore(button, insertionDiv.nextSibling);
+    insertionDiv.appendChild(button);
+
+    // JavaScript Setup
+    $(function () {
+        $( "#dialog1" ).dialog({
+            autoOpen: false,
+            width: width + spacing,
+            height: height + spacing + 45 // 40px to compensate for menu
+        });
+
+        $("#opener").click(function() {
+            $("#dialog1").dialog('open');
+        });
+    });
+}
